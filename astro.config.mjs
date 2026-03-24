160d9c50a936b8dbb292f1d2d4469b71ad17e1ff
Michal Cisek <mcisek93@gmail.com>
2026-03-24 23:57:42 +0100

    add background on main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,8 +13,8 @@ export default defineConfig({
     rehypePlugins: [[rehypeKatex, { strict: false }]],
     shikiConfig: {
       themes: {
-        light: 'github-light',
-        dark: 'github-dark',
+        light: 'catppuccin-latte',
+        dark: 'catppuccin-mocha',
       },
     },
   },
